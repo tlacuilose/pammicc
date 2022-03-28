6e564f4424fe1deb6d4a450f1e44b4333bb22793
Jose Tlacuilo <65783495+tlacuilose@users.noreply.github.com>
2022-03-28 14:54:20 -0600

    Feature/client digital ocean (#8)
    Preparations for swift deployment, changes exports and set environmment variables.

--- client/src/data/datasources/pammicc-api.js
+++ client/src/data/datasources/pammicc-api.js
@@ -1,33 +1,32 @@
-const server_url =  'http://localhost:5005';
+const server_url =  process.env.REACT_APP_API_URL
 
-module.exports = {
-  getProjects: async function() {
+export async function getProjects () {
     const response = await fetch(`${server_url}/projects`).catch(error => {
-      throw error;
-    });
+    throw error;
+  });
 
-    if (!response.ok) {
-      throw new Error("An error ocurred fetching projects");
-    }
+  if (!response.ok) {
+    throw new Error("An error ocurred fetching projects");
+  }
 
-    const json = await response.json();
-    
-    if (!json) {
-      throw new Error("An error occured getting json data from fetching projects.");
-    }
+  const json = await response.json();
+  
+  if (!json) {
+    throw new Error("An error occured getting json data from fetching projects.");
+  }
 
-    return json;
-  },
-  newProject: async function(newProject) {
-    await fetch(`${server_url}/projects/new`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newProject),
-    })
-    .catch(error => {
-      throw error;
-    })
-  },
-};
+  return json;
+}
+
+export async function newProject(newProject) {
+  await fetch(`${server_url}/projects/new`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newProject),
+  })
+  .catch(error => {
+    throw error;
+  })
+}
--- client/src/data/repositories/projects-repository.js
+++ client/src/data/repositories/projects-repository.js
@@ -16,56 +16,55 @@ class Project {
   }
 }
 
-module.exports = {
-  getProjects: async function() {
-    try {
-      let projects = [];
-      const response = await ds.getProjects();
-      if (Array.isArray(response)) {
-        response.map( project => {
-          projects.push(
-            new Project(
-              project._id,
-              project.name,
-              project.description,
-              project.url,
-              project.tags
-            )
+export async function getProjects() {
+  try {
+    let projects = [];
+    const response = await ds.getProjects();
+    if (Array.isArray(response)) {
+      response.map( project => {
+        projects.push(
+          new Project(
+            project._id,
+            project.name,
+            project.description,
+            project.url,
+            project.tags
           )
-        });
-        return {
-          result: projects,
-          error: null
-        };
-      }
+        )
+      });
       return {
-        result: null,
-        error: new Error("An error occurred getting projects from api-datasource.")
+        result: projects,
+        error: null
       };
-    } catch (e) {
-      return {
-        result: null,
-        error: e
-      }
     }
-  },
-  addNewProject: async function(values) {
-    try {
-      const newProject = new Project(
-        null,
-        values.name,
-        values.description,
-        values.url,
-        values.tags
-      );
-      newProject.validate();
+    return {
+      result: null,
+      error: new Error("An error occurred getting projects from api-datasource.")
+    };
+  } catch (e) {
+    return {
+      result: null,
+      error: e
+    }
+  }
+}
 
-      await ds.newProject(newProject);
-      return null;
-    } catch (error) {
-      return error;
-    }
-  },
-};
+export async function addNewProject(values) {
+  try {
+    const newProject = new Project(
+      null,
+      values.name,
+      values.description,
+      values.url,
+      values.tags
+    );
+    newProject.validate();
+
+    await ds.newProject(newProject);
+    return null;
+  } catch (error) {
+    return error;
+  }
+}
 
 
