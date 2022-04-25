9b3264c3ccabfe616606981f23e7210864ac6505
Jose Tlacuilo <tlacuilose@outlook.com>
2022-04-24 21:25:45 -0500

    Edits owned projects.

--- server/routes/project.js
+++ server/routes/project.js
@@ -25,6 +25,7 @@ projectRoutes.route("/projects").get(function (req, res) {
 
 // Get a single project by id
 projectRoutes.route("/projects/:id").get(function (req, res) {
+  console.log("Trying get");
   let db_connect = dbo.getDb();
   let id_query = { _id: ObjectId(req.params.id)};
   db_connect
@@ -34,58 +35,5 @@ projectRoutes.route("/projects/:id").get(function (req, res) {
       res.json(result);
     });
 });
-
-// ------------------------------------------------------------------------------
-
-// MOVED to /secureRoutes
-
-// // Upload a new project.
-// projectRoutes.route("/projects/new").post(function (req, response) {
-//   let db_connect = dbo.getDb();
-//   let project = {
-//     name: req.body.name,
-//     description: req.body.description,
-//     url: req.body.url,
-//     tags: req.body.tags,
-//   };
-//   db_connect.collection("projects").insertOne(project, function (err, res) {
-//     if (err) throw err;
-//     response.json(res);
-//   });
-// });
-
-// -------------------------------------------------------------------------------
-
-// Update a project by id.
-/* TODO: Add update and delete queries.
-projectRoutes.route("record/update/:id").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let id_query = { _id: ObjectId( req.params.id )};
-  let new_values = {
-    $set: {
-      project_name: req.body.project_name,
-      project_description: req.body.project_description,
-    },
-  };
-  db_connect
-    .collection("projects")
-    .updateOne(id_query, new_values, function (err, res) {
-      if (err) throw err;
-      console.log("1 document updated")
-      respones.json(res);
-    });
-});
-
-// Delete a project
-projectRoutes.route("project/:id").delete(function (req, response) {
-  let db_connect = dbo.getDb();
-  let id_query = { _id: ObjectId( req.params.id )};
-  db_connect.collection("projects").deleteOne(id_query, function (err, obj) {
-    if (err) throw err;
-    console.log("1 document deleted");
-    response.json(obj);
-  });
-});
-*/
 
 module.exports = projectRoutes;
